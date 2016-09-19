b44711d9bd631f787407624d3e7c7264a88eb680
ac360 <austen@serverless.com>
2016-09-18 18:10:36 -0700

    clean out bloated comments

--- handler.js
+++ handler.js
@@ -4,5 +4,3 @@
 module.exports.hello = (event, context, cb) => {
   cb(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
-
-// You can add more handlers here, and reference them in serverless.yml
